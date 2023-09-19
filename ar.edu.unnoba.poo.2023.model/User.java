9d5dc43407ad8766af4c58adc409ed9e307dba2e
LucasCoria12 <lucaslcoria122@gmail.com>
2023-09-19 15:47:59 -0300

    agrege la carpeta ar.edu.unnoba.poo.2023.model y cree dentro la clase User

---
+++ ar.edu.unnoba.poo.2023.model/User.java
@@ -0,0 +1,15 @@
+public class User {
+   private String email;
+   private String password; 
+
+   public User(){}
+
+   public String getEmail(){
+    return this.email;
+   }
+
+   public String getPassword(){
+    return this.password;
+   }
+
+}
